2a56e5c19419e61353f0bdca70d41da0a81d6f67
Huongtt2712 <tietthuhuong252728@gmail.com>
2024-01-15 04:25:11 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,84 @@
+// // Ways to declare a variable
+// // `var`, `let`, & `const`
+
+// let firstName = "John";
+// const lastName = "Doe";
+// let age = 30;
+
+// console.log(age);
+
+// // Naming Conventions
+// // - Only letters, numbers, underscores and dollar signs
+// // - Can't start with a number
+
+// // Multi-Word Formatting
+// // firstName = camelCase
+// // first_name = underscore
+// // FirstName = PascalCase
+// // firstname = lowercase
+
+// // We can re-assign `let` variables. If you change `age` to use `const`, you will get an error
+// age = 31;
+// console.log(age);
+
+// // With let, we can declare a value without assigning a value
+// let score;
+// score = 1;
+// console.log(score);
+
+// if (true) {
+//   score = score + 1;
+// }
+
+// console.log(score);
+
+// const x = 100;
+
+// // We can not re-assign a const variable
+// // x = 200 // Will result in an error
+
+// // We can still manipulate arrays and objects using const
+// const arr = [1, 2, 3, 4];
+// arr.push(5);
+// console.log(arr);
+
+// const person = {
+//   name: "huong",
+// };
+// person.name = "huong";
+// person.email = "huong@gmail.com";
+// console.log(person);
+
+// // Declare multiple values at once
+// let a, b, c;
+
+// const d = 10,
+//   e = 20,
+//   f = 30;
+
+// console.log(d);
+// console.log(a);
+
+
+//bài số 4 : data type
+const firstName = "Sara"; //string "|"
+
+const age = 30; // number 
+const temp = 98.9;
+
+const hasKids = true; // boolean 
+
+const aptNumber = null;// Undefined
+
+// phân biet null và Undefined
+console.log(score);
+//array [giatri1,giatri2]
+const numbers = [1, 2, 3, 4];
+
+//object
+const person = {
+  name: "Brad",
+};
+
+
+const score = undefined;
